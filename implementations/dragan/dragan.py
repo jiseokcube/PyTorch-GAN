327bd10193dd57e43bfce76a9077bed5766f10ee
jiseokcube <jiseokcube@gmail.com>
2019-04-29 02:35:51 -0400

    Fixed namespace bug in DRAGAN

--- implementations/dragan/dragan.py
+++ implementations/dragan/dragan.py
@@ -222,7 +222,7 @@ for epoch in range(opt.n_epochs):
 #            % (epoch, opt.n_epochs, i, len(mnist_loader), d_loss.item(), g_loss.item())
 #        )
         batches_done = epoch * len(dataloader) + i
-        if batches_done % sample_interval == 0:
+        if batches_done % opt.sample_interval == 0:
             print(
                 "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
                 % (epoch, n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
